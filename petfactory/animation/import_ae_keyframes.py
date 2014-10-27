fe5291409262ea03d8e6e6af8a2f9c0d301ff44e
Johan Borgström <johan@petfactory.se>
2014-10-27 09:46:04 +0100

    added support for focal length, correct rot order

--- petfactory/animation/import_ae_keyframes.py
+++ petfactory/animation/import_ae_keyframes.py
@@ -6,34 +6,44 @@ import math
 
 # read the json
 data = None
-with open('/Users/johan/Desktop/_data_v04.json', 'r') as f:
+with open('/Users/johan/Desktop/_data_v09.json', 'r') as f:
     data = f.read()
 
 #print(data)
 
 json_data = json.loads(data)
-null_list = json_data.get('null')
-cam_list = json_data.get('camera')
+null_list = json_data.get('nulls')
+cam_list = json_data.get('cameras')
 
-    
+rotate_orders = {'xyz':0, 'yzx':1, 'zxy':2, 'xzy':3, 'yxz':4, 'zyx':5} 
+
+  
 def set_keyframes_from_matrix_lists(node_list, node_type):
         
     for node_dict in node_list:
         
         for name, attr_dict in node_dict.iteritems():
     
+            rot_order = attr_dict.get('rotate_order')
+            
             if node_type is 'null':
                 node = pm.spaceLocator(n=name)
+                node.rotateOrder.set(rot_order)
                 
             elif node_type is 'camera':
                 node = pm.camera()[0]
+                node.rotateOrder.set(rot_order)
+                cam_shape = node.getShape()
+                focal_length_list = attr_dict.get('focal_length')
+                
         
             matrix_list = attr_dict.get('matrix')
             
             for index, m in enumerate(matrix_list):
                 
                 # create a open maya transformation matrix                         
-                matrix = om.MMatrix([m[0][0], m[0][1], m[0][2], 0, m[1][0], m[1][1], m[1][2], 0, m[2][0], m[2][1], m[2][2], 0, m[3][0], m[3][1], m[3][2], 1])
+                #matrix = om.MMatrix([m[0][0], m[0][1], m[0][2], 0, m[1][0], m[1][1], m[1][2], 0, m[2][0], m[2][1], m[2][2], 0, m[3][0], m[3][1], m[3][2], 1])
+                matrix = om.MMatrix([m[0], m[1], m[2], 0, m[3], m[4], m[5], 0, m[6], m[7], m[8], 0, m[9], m[10], m[11], 1])
                 t_matrix = om.MTransformationMatrix(matrix)
                 
                 # get the euler rotation from the matrix (default rotation order is XYZ)
@@ -44,7 +54,6 @@ def set_keyframes_from_matrix_lists(node_list, node_type):
                 
                 # get the translation
                 translation = t_matrix.translation(1)
-                
                 
                 # convert radians to degrees
                 rx = (180/math.pi)*rot_zyx[0]
@@ -64,10 +73,17 @@ def set_keyframes_from_matrix_lists(node_list, node_type):
                 
                 # set some keyframes
                 pm.setKeyframe(node, attribute=['translate', 'rotate'], t=index)
+                
+
+                if node_type is 'camera':
+                    
+                    cam_shape.focalLength.set(focal_length_list[index])
+                    pm.setKeyframe(cam_shape, attribute='focalLength', t=index)
+
             
             
 set_keyframes_from_matrix_lists(null_list, 'null')
-#set_keyframes_from_matrix_lists(cam_list, 'camera')
+set_keyframes_from_matrix_lists(cam_list, 'camera')
 
 '''
 m = null_list[0].get('Null 1').get('matrix')[24]
