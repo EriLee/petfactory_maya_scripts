806441ebb57ff06ab97a61c5aabb85aeb3bf2be6
Johan Borgström <johan@petfactory.se>
2014-11-24 22:52:08 +0100

    polyFacet

--- petfactory/modelling/curves/rot_m.py
+++ petfactory/modelling/curves/rot_m.py
@@ -1,19 +1,38 @@
 import pymel.core as pm
+
 '''
-m = [  [1,0,0,0],
+m_1 = [  [1,0,0,0],
         [0,1,0,0],
         [0,0,1,0],
         [0,0,0,1],
 ]
 '''
 
-m = pm.ls(sl=True)[0].getMatrix()
-tm = pm.datatypes.TransformationMatrix(m)
+m_1 = pm.ls(sl=True)[0].getMatrix(worldSpace=True)
+tm = pm.datatypes.TransformationMatrix(m_1)
+pos = tm.getTranslation(space='world')
 
-vec = pm.datatypes.Vector(1,0,0);
+vec_a = pm.datatypes.Vector(-1,0,-1);
+vec_b = pm.datatypes.Vector(1,0,-1);
+vec_c = pm.datatypes.Vector(1,0,1);
+vec_d = pm.datatypes.Vector(-1,0,1);
 
-vec_rot = vec.rotateBy(tm)
+vec_a_rot = vec_a.rotateBy(tm) + pos
+vec_b_rot = vec_b.rotateBy(tm) + pos
+vec_c_rot = vec_c.rotateBy(tm) + pos
+vec_d_rot = vec_d.rotateBy(tm) + pos
 
 loc = pm.spaceLocator()
-loc.translate.set(vec_rot)
+loc.translate.set(vec_a_rot)
+
+loc = pm.spaceLocator()
+loc.translate.set(vec_b_rot)
+
+loc = pm.spaceLocator()
+loc.translate.set(vec_c_rot)
+
+loc = pm.spaceLocator()
+loc.translate.set(vec_d_rot)
+
+pm.polyCreateFacet(p=[vec_a_rot, vec_b_rot, vec_c_rot, vec_d_rot])
 
