a95f310257a62a8a146e67c870c7ed4476c54652
Johan Borgström <johan@petfactory.se>
2014-11-30 17:48:13 +0100

    add circle

--- petfactory/modelling/curves/project_dot_cross_test.py
+++ petfactory/modelling/curves/project_dot_cross_test.py
@@ -14,7 +14,8 @@ sel_all = pm.ls(type='transform')
 for x in sel_all:
     if x.name() != 'crv':
         pm.delete(x)  
-    
+ 
+radius = .75   
 # get the curve
 crv = pm.PyNode('crv')
 # get the cv positions
@@ -42,15 +43,25 @@ up_vec_ortho = abc_cross.cross(aim_vec)
 # "project" the vector on the ortogonal vectors using the dot product
 # i.e. find out how much the vector points in the same direction as the
 # orthogonal vector
+aim_u = aim_vec.dot(aim_vec)
+aim_v = aim_vec.dot(up_vec_ortho)
+
 up_u = up_vec.dot(aim_vec)
 up_v = up_vec.dot(up_vec_ortho)
 
-aim_u = aim_vec.dot(aim_vec)
-aim_v = aim_vec.dot(up_vec_ortho)
+# calculate the mid vector
+mid_u = aim_u + up_u
+mid_v = aim_v + up_v
+mag = math.sqrt(mid_u*mid_u + mid_v*mid_v)
+mid_u_n = mid_u/mag
+mid_v_n = mid_v/mag
+# create a pymel vector
+mid_vec = pm.datatypes.Vector(mid_u_n, mid_v_n, 0)
 
 # visualize the uv pos
 pos(up_u, up_v)
 pos(aim_u, aim_v)
+pos(mid_u_n, mid_v_n)
 
 
 #build a transformation matrix to transform vectors 
@@ -59,10 +70,9 @@ tm = pm.datatypes.TransformationMatrix(
 [aim_vec[0],aim_vec[1],aim_vec[2],0],
 [up_vec_ortho[0], up_vec_ortho[1], up_vec_ortho[2],0],
 [abc_cross[0], abc_cross[1], abc_cross[2], 0],
-
-
 [0,0,0,1])
 
+'''
 vec_test = pm.datatypes.Vector(up_u, up_v, 0)
 rot_v = vec_test.rotateBy(tm)
 pos(rot_v.x, rot_v.y, rot_v.z)
@@ -70,18 +80,34 @@ pos(rot_v.x, rot_v.y, rot_v.z)
 vec_test_2 = pm.datatypes.Vector(aim_u, aim_v, 0)
 rot_v_2 = vec_test_2.rotateBy(tm)
 pos(rot_v_2.x, rot_v_2.y, rot_v_2.z)
-
-
+'''
 
 # calculate the theta angle
-theta_aim = math.atan2(aim_v, aim_u)
-theta_up = math.atan2(up_v, up_u)
+theta_ba = math.atan2(aim_v, aim_u)   # aim
+theta_bc = math.atan2(up_v, up_u)     # up
 
+'''
 aim_line = line([(0,0,0),(5,0,0)], name='aim_vec')
 aim_line.rotate.set(0,0,pm.util.degrees(theta_aim))
     
 up_line = line([(0,0,0),(5,0,0)], name='up_vec')
 up_line.rotate.set(0,0,pm.util.degrees(theta_up))
+'''
 
+if theta_ba < theta_bc:
+    theta_rad = (theta_bc - theta_ba)/2
+else:
+    theta_rad = (theta_ba - theta_bc)/2
 
+# get the length of the hypotenuse    
+h = radius / math.sin(theta_rad)
+# get the length of the adjacent 
+a = radius / math.tan(theta_rad)
 
+# get the circle cenyter position
+circ_center = mid_vec*h
+circ_center_tm = circ_center.rotateBy(tm)
+
+circ = pm.circle(r=radius, normal=(0,0,1))[0]
+circ.setMatrix(tm)
+circ.translate.set(circ_center_tm)
