8ed2e8fb148b4a6c1155fec89c95f580e5e4a97d
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-01 10:24:14 +0100

    add pos on the circle perim

--- petfactory/modelling/curves/project_dot_cross_test.py
+++ petfactory/modelling/curves/project_dot_cross_test.py
@@ -5,7 +5,7 @@ def line(pos_list, name=''):
     return pm.curve(degree=1, p=pos_list, name=name)
     
 def pos(x, y, z=0):
-    sp = pm.polySphere(r=.1)[0]
+    sp = pm.polySphere(r=.05)[0]
     sp.translate.set((x, y, z))
     return sp
     
@@ -15,7 +15,7 @@ for x in sel_all:
     if x.name() != 'crv':
         pm.delete(x)  
  
-radius = .75
+radius = 2
 # get the curve
 crv = pm.PyNode('crv')
 # get the cv positions
@@ -62,12 +62,7 @@ def circular_corners(pos_list):
     mid_u_n = mid_u/mag
     mid_v_n = mid_v/mag
     # create a pymel vector
-    mid_vec = pm.datatypes.Vector(mid_u_n, mid_v_n, 0)
-    
-    # visualize the uv pos
-    #pos(up_u, up_v)
-    #pos(aim_u, aim_v)
-    #pos(mid_u_n, mid_v_n)
+    mid_vec = pm.datatypes.Vector(mid_u_n, mid_v_n, 0) 
     
     
     #build a transformation matrix to transform vectors 
@@ -77,7 +72,7 @@ def circular_corners(pos_list):
     [up_vec_ortho[0], up_vec_ortho[1], up_vec_ortho[2],0],
     [abc_cross[0], abc_cross[1], abc_cross[2], 0],
     [0,0,0,1])
-
+    
     
     # calculate the theta angle
     theta_ba = math.atan2(aim_v, aim_u)   # aim
@@ -96,6 +91,69 @@ def circular_corners(pos_list):
     circle_center = pos_list[1] + (mid_vec*h).rotateBy(tm)
     pos(circle_center.x, circle_center.y, circle_center.z)
     
+
+    
+    ##########################################
+    #                                        #
+    #    Visualize the world space vectors   # 
+    #                                        #
+    ##########################################
+    
+    
+    # visualize the vectors oriented to world space
+    #o = (0,0,0)
+    #line([o, aim_uv_vec*5])
+    #line([o, up_uv_vec*5])
+    #line([o, mid_vec*5])
+    # the circle center pos
+    #pos((mid_vec*h).x, (mid_vec*h).y)
+    
+    
+
+    ##########################################
+    #                                        #
+    #    positions on the circle perimeter   #
+    #                                        #
+    ##########################################
+    
+    
+    # the angle we need to rotate from positive x axis to the theta angle
+    ang = math.pi + theta_rad
+
+    # the angle between theta and the oposite side of the right triangle (1.5 PI)
+    # the opposite side are perpendicular to the adjacent side that intersects the circle
+    diff_ang = math.pi*1.5 - ang
+    
+    # number of point we want on the circle perimeter (will be reflected across the mid vector)
+    num_points = 1
+    ang_inc = diff_ang/(num_points+1)
+    mid_vec_scaled = mid_vec*h
+    
+    for i in range(num_points):
+
+        rot = ang + ang_inc * (i+1)
+        
+        x = (math.cos(rot)*radius) + (mid_vec_scaled).x
+        y = (math.sin(rot)*radius) + (mid_vec_scaled).y
+     
+        p = pm.datatypes.Vector(x,y,0)
+        
+        # reflect around the mid vec
+        p_refl = 2 * p.dot(mid_vec_scaled) / mid_vec_scaled.dot(mid_vec_scaled) * mid_vec_scaled - p
+        
+        # visualize the pos along circle perimeter
+        #pos(p.x, p.y, p.z)
+        #pos(p_refl.x, p_refl.y, p_refl.z)
+        
+        
+        p_tm = p.rotateBy(tm) + pos_list[1]
+        p_refl_tm = p_refl.rotateBy(tm) + pos_list[1]
+        
+        pos(p_tm.x, p_tm.y, p_tm.z)
+        pos(p_refl_tm.x, p_refl_tm.y, p_refl_tm.z)
+        
+
+    
     #h_vec = pm.datatypes.Vector(h, 0, 0)
     #h_vec_tm = pos_list[1] + h_vec.rotateBy(tm)
     #pos(h_vec_tm.x, h_vec_tm.y, h_vec_tm.z)
@@ -104,8 +162,14 @@ def circular_corners(pos_list):
     adjacent_aim = (aim_uv_vec.normal() * a).rotateBy(tm) + pos_list[1]
     pos(adjacent_aim.x, adjacent_aim.y, adjacent_aim.z)
     
+    # could also reflect to get this position
     adjacent_up = (up_uv_vec.normal() * a).rotateBy(tm) + pos_list[1]
     pos(adjacent_up.x, adjacent_up.y, adjacent_up.z)
+    
+    
+    
+    
+    
     
     # get the circle cenyter position
     circ_center = (mid_vec*h).rotateBy(tm)
