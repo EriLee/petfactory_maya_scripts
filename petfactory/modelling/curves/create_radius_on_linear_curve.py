10c749a4b531f6809bc3be75af044b7543598d7b
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-02 09:33:49 +0100

    radius_on_linear..  refactor

--- petfactory/modelling/curves/create_radius_on_linear_curve.py
+++ petfactory/modelling/curves/create_radius_on_linear_curve.py
@@ -4,13 +4,14 @@ import math, pprint
 def line(pos_list, name=''):
     return pm.curve(degree=1, p=pos_list, name=name)
     
-def pos(x, y, z=0):
-    sp = pm.polySphere(r=.3)[0]
+def pos(x, y, z=0, name='pos'):
+    sp = pm.polySphere(r=.06, name=name)[0]
     sp.translate.set((x, y, z))
     return sp
     
 def create_round_corner(pos_list, radius):
     
+    # the position return list
     ret_pos_list = []
     
     # define the vectors
@@ -21,16 +22,11 @@ def create_round_corner(pos_list, radius):
     aim_vec = ba_vec.normal()
     up_vec = bc_vec.normal()
     
-    # get the cross
-    abc_cross = ba_vec.cross(bc_vec).normal()
+    # get the cross product of aim and up
+    cross_vec = ba_vec.cross(bc_vec).normal()
     
     # make sure the up vector is orthogonal
-    up_vec_ortho = abc_cross.cross(aim_vec)
-    
-    # visualize the ortho vectors
-    #o = (0,0,0)
-    #line([o, aim_vec])
-    #line([o, up_vec_ortho])
+    up_vec_ortho = cross_vec.cross(aim_vec)
     
     # "project" the vector on the ortogonal vectors using the dot product
     # i.e. find out how much the vector points in the same direction as the
@@ -41,151 +37,116 @@ def create_round_corner(pos_list, radius):
     up_u = up_vec.dot(aim_vec)
     up_v = up_vec.dot(up_vec_ortho)
     
+    # create pymel vectors
     aim_uv_vec = pm.datatypes.Vector(aim_u, aim_v, 0)
     up_uv_vec = pm.datatypes.Vector(up_u, up_v, 0)
+
+    # cretae the mid vector
+    mid_vec = (aim_uv_vec + up_uv_vec).normal()
     
-    # calculate the mid vector
-    mid_u = aim_u + up_u
-    mid_v = aim_v + up_v
-    mag = math.sqrt(mid_u*mid_u + mid_v*mid_v)
-    mid_u_n = mid_u/mag
-    mid_v_n = mid_v/mag
-    # create a pymel vector
-    mid_vec = pm.datatypes.Vector(mid_u_n, mid_v_n, 0) 
-    
-    
-    #build a transformation matrix to transform vectors 
+    # visulaize the local vectors
+    #line([(0,0,0), aim_uv_vec])
+    #line([(0,0,0), up_uv_vec])
+    #line([(0,0,0), mid_vec])
+
+    # build a transformation matrix to transform vectors 
     # from world space to the rotated space
     tm = pm.datatypes.TransformationMatrix(
     [aim_vec[0],aim_vec[1],aim_vec[2],0],
     [up_vec_ortho[0], up_vec_ortho[1], up_vec_ortho[2],0],
-    [abc_cross[0], abc_cross[1], abc_cross[2], 0],
-    [0,0,0,1])
-    
-    
-    # calculate the theta angle
-    theta_ba = math.atan2(aim_v, aim_u)   # aim
-    theta_bc = math.atan2(up_v, up_u)     # up
-    
-    if theta_ba < theta_bc:
-        theta_rad = (theta_bc - theta_ba)/2
-    else:
-        theta_rad = (theta_ba - theta_bc)/2
+    [cross_vec[0], cross_vec[1], cross_vec[2], 0],
+    #[0,0,0,1])
+    [pos_list[1][0],pos_list[1][1],pos_list[1][2],1])
+      
+    # calculate the theta angle, remember atan2(y, x) or (v, u)
+    theta = math.atan2(mid_vec.y, mid_vec.x)  
+    #print(theta)
     
     # get the length of the hypotenuse    
-    h = radius / math.sin(theta_rad)
+    h = radius / math.sin(theta)
     # get the length of the adjacent 
-    a = radius / math.tan(theta_rad)
-    
-    circle_center = pos_list[1] + (mid_vec*h).rotateBy(tm)
-    #pos(circle_center.x, circle_center.y, circle_center.z)
-    
-
-    
-    ##########################################
-    #                                        #
-    #    Visualize the world space vectors   # 
-    #                                        #
-    ##########################################
-    
-    
-    # visualize the vectors oriented to world space
-    #o = (0,0,0)
-    #line([o, aim_uv_vec*5])
-    #line([o, up_uv_vec*5])
-    #line([o, mid_vec*5])
-    # the circle center pos
-    #pos((mid_vec*h).x, (mid_vec*h).y)
-    
-    
-
-    ##########################################
-    #                                        #
-    #    positions on the circle perimeter   #
-    #                                        #
-    ##########################################
-    
-    
+    a = radius / math.tan(theta)
+          
     # the angle we need to rotate from positive x axis to the theta angle
-    ang = math.pi + theta_rad
+    ang = math.pi + theta
 
     # the angle between theta and the oposite side of the right triangle (1.5 PI)
     # the opposite side are perpendicular to the adjacent side that intersects the circle
     diff_ang = math.pi*1.5 - ang
     
     # number of point we want on the circle perimeter (will be reflected across the mid vector)
+    # needs to be at least 2
     num_points = 5
-    ang_inc = diff_ang/(num_points+1)
+    
+    ang_inc = diff_ang/(num_points-1)
     mid_vec_scaled = mid_vec*h
     
+    # gather pooint on the circle perimeter
     pos_on_circ = []
-    pos_on_circ_reflect = []
     for i in range(num_points):
 
-        rot = ang + ang_inc * (i+1)
-        
+        rot = ang + ang_inc * (i)
         x = (math.cos(rot)*radius) + (mid_vec_scaled).x
         y = (math.sin(rot)*radius) + (mid_vec_scaled).y
      
         p = pm.datatypes.Vector(x,y,0)
-        p_tm = p.rotateBy(tm) + pos_list[1]
-        # visualize
-        #pos(p_tm.x, p_tm.y, p_tm.z)
-        pos_on_circ.append(p_tm)
+        pos_on_circ.append(p)
         
+
+    # copy and reflect the positions on the circle to get the positions on the circle reflected by the mid vector
+    pos_on_circ_reflected = []
+    for i in range(1, num_points):
+
         # reflect around the mid vec
-        p_refl = 2 * p.dot(mid_vec_scaled) / mid_vec_scaled.dot(mid_vec_scaled) * mid_vec_scaled - p   
-        p_refl_tm = p_refl.rotateBy(tm) + pos_list[1] 
-        #pos(p_refl_tm.x, p_refl_tm.y, p_refl_tm.z)
-        pos_on_circ_reflect.append(p_refl_tm)
+        pos_on_circ_reflected.append(2 * pos_on_circ[i].dot(mid_vec) / mid_vec.dot(mid_vec) * mid_vec - pos_on_circ[i])
+ 
+    # reverse the first pos on circle
+    pos_on_circ.reverse()
+    
+    # merge the pos lists
+    ret_pos_list = pos_on_circ[:]
+    ret_pos_list += pos_on_circ_reflected 
+ 
+
+    #for p in ret_pos_list:
+    #    pos(p.x, p.y, p.z)
         
-        # visualize the pos along circle perimeter
-        #pos(p.x, p.y, p.z)
-        #pos(p_refl.x, p_refl.y, p_refl.z)
+    # transform the position to world space  
+    for i, p in enumerate(ret_pos_list):
+        ret_pos_list[i] = p.rotateBy(tm) + pos_list[1]
         
-    
-    
-    #h_vec = pm.datatypes.Vector(h, 0, 0)
-    #h_vec_tm = pos_list[1] + h_vec.rotateBy(tm)
-    #pos(h_vec_tm.x, h_vec_tm.y, h_vec_tm.z)
+    for p in ret_pos_list:
+        pos(p.x, p.y, p.z)
         
-    # get the positions where the circle touches the lines
-    adjacent_aim = (aim_uv_vec.normal() * a).rotateBy(tm) + pos_list[1]
-    #pos(adjacent_aim.x, adjacent_aim.y, adjacent_aim.z)
+
+    #---------------------
     
-    # could also reflect to get this position
-    adjacent_up = (up_uv_vec.normal() * a).rotateBy(tm) + pos_list[1]
-    #pos(adjacent_up.x, adjacent_up.y, adjacent_up.z)
-    
-    
+    # visualize:
+        
     # get the circle cenyter position
     circ_center = (mid_vec*h).rotateBy(tm)
     circ_center_tm = circ_center
+ 
+    # get the positions where the circle touches the lines
+    adjacent_aim = aim_uv_vec.normal() * a
+    adjacent_aim_refl = 2 * adjacent_aim.dot(mid_vec) / mid_vec.dot(mid_vec) * mid_vec - adjacent_aim
+
+    # visulaize the local vectors
+    adjacent_aim_tm = adjacent_aim.rotateBy(tm) + pos_list[1]
+    adjacent_aim_refl_tm = adjacent_aim_refl.rotateBy(tm) + pos_list[1]
     
-    #circ = pm.circle(r=radius, normal=(0,0,1))[0]
-    #circ.setMatrix(tm)
-    #circ.translate.set(pos_list[1]+circ_center_tm)
     
-    # the position where the mid line first intersects the circle
-    mid_pos_on_circ = mid_vec*(h-radius)
-    mid_pos_on_circ_tm = mid_pos_on_circ.rotateBy(tm)+pos_list[1]
+    circ = pm.circle(r=radius, normal=(0,0,1))[0]
+    circ.setMatrix(tm)
+    circ.translate.set(pos_list[1]+circ_center_tm)
     
+    line([pos_list[1]+circ_center_tm, adjacent_aim_tm])
+    line([pos_list[1]+circ_center_tm, adjacent_aim_refl_tm])
+    line([pos_list[1], pos_list[1]+circ_center_tm])
+
+    #---------------------
     
-    #pos(mid_pos_on_circ_tm.x, mid_pos_on_circ_tm.y, mid_pos_on_circ_tm.z)
-    
-    #line([pos_list[1], pos_list[1]+circ_center])
-    #line([adjacent_aim, pos_list[1]+circ_center])
-    #line([adjacent_up, pos_list[1]+circ_center])
-    
-    # reverse the list of positions on the curves
-    pos_on_circ.reverse()
-    
-    ret_pos_list.append(adjacent_aim)
-    ret_pos_list += pos_on_circ
-    ret_pos_list.append(mid_pos_on_circ_tm)
-    ret_pos_list += pos_on_circ_reflect
-    ret_pos_list.append(adjacent_up)
-    
+             
     return ret_pos_list
     
     
