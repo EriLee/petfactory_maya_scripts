c770f6e706df505b6d967b84565a9c591c354b92
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-05 11:21:48 +0100

    init pipe_tool

---
+++ petfactory/modelling/pipe_tool/local_to_world.py
@@ -0,0 +1,32 @@
+import pymel.core as pm
+import pprint
+
+loc_o = pm.PyNode('locator1')
+loc_t = pm.PyNode('locator2')
+
+world_m = loc_t.getMatrix()
+world_tm = pm.datatypes.TransformationMatrix(world_m)
+
+
+#loc_om = loc_o.getMatrix()
+#x = loc_om[0][0]
+#y = loc_om[0][1]
+
+lp = loc_o.getTranslation(s='world')
+x = lp.x
+y = lp.y
+
+for z in range(10):
+    
+    local_vector = pm.datatypes.Vector(x, y, z)
+    print(local_vector)
+    
+    vector_in_world = local_vector.rotateBy(world_tm)
+
+    z_wp = vector_in_world + world_tm.getTranslation(space='world')
+    
+    sl = pm.spaceLocator()
+    sl.translate.set(z_wp)
+        
+
+
