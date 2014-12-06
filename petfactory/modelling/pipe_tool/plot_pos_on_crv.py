6b5a3ae5cb5e84136965cc9b3a8f4b040d0827bd
Johan Borgström <johan@petfactory.se>
2014-12-06 12:52:29 +0100

    wrap in a def

--- petfactory/modelling/pipe_tool/plot_pos_on_crv.py
+++ petfactory/modelling/pipe_tool/plot_pos_on_crv.py
@@ -12,79 +12,89 @@ def pos(p, size=.2):
     loc.localScale.set((size,size,size))
     loc.translate.set(p)
 
-radius = 3
-theta = math.pi/6
-num_points = 6
+def build_radius_t_matrix(radius, theta, num_points):
 
-# The total angle in a triangle is 180 degrees
-# since one angle is 90 deg (pi/2) and the theta ang is known, we can
-# get the remaining angle with 180 - 90 - theta, which can be simplified to
-# 90 - theta i.e. (math.pi/2) - theta
-ang_opp = (math.pi/2) - theta
-
-# get the adjacent given the opposite (radius) and the angle theta
-# math.tan(theta) = o/a
-# a * math.tan(theta) = o
-# a = a / math.tan(theta)
-adjacent = radius / math.tan(theta)
-
-# create the vector to reflect about
-mid_vec = pm.datatypes.Vector(adjacent, radius, 0)
-
-radius_center = pm.datatypes.Vector(adjacent, radius, 0)
-
-# calculate the positions on the circle
-ang_inc = ang_opp/(num_points-1)
-
-pos_on_circ = []
-for i in range(num_points):
+    #radius = 3
+    #theta = math.pi/6
     
-    a = (1.5 * math.pi) - (ang_inc * i)
-    x = math.cos(a) * radius + adjacent
-    y = math.sin(a) * radius + radius
-    pos_on_circ.append(pm.datatypes.Vector(x, y, 0))
+    # The total angle in a triangle is 180 degrees
+    # since one angle is 90 deg (pi/2) and the theta ang is known, we can
+    # get the remaining angle with 180 - 90 - theta, which can be simplified to
+    # 90 - theta i.e. (math.pi/2) - theta
+    ang_opp = (math.pi/2) - theta
     
-
-# reflect the pos on circle across the mid vec  
-pos_on_circ_reflected = [ 2 * pos_on_circ[i].dot(mid_vec) / mid_vec.dot(mid_vec) * mid_vec - pos_on_circ[i] for i in range(num_points-1) ]
-
-# combine the pos and the reflected pos
-pos_on_circ_reflected.reverse()
-radius_pos_list =  pos_on_circ + pos_on_circ_reflected
-
-
-# create transformation matrices per point on circle
-t_matrix_list = []
-for p in radius_pos_list:
-    aim = (radius_center - p).normal()
-    up = pm.datatypes.Vector(0,0,1)
-    cross = aim.cross(up)
+    # get the adjacent given the opposite (radius) and the angle theta
+    # math.tan(theta) = o/a
+    # a * math.tan(theta) = o
+    # a = a / math.tan(theta)
+    adjacent = radius / math.tan(theta)
     
-    tm = pm.datatypes.TransformationMatrix( [ [aim.x, aim.y, aim.z, 0],
-                                              [cross.x, cross.y, cross.z, 0],
-                                              [up.x, up.y, up.z, 0],
-                                              [p.x, p.y, p.z, 1] ])
-    t_matrix_list.append(tm)
+    # create the vector to reflect about
+    mid_vec = pm.datatypes.Vector(adjacent, radius, 0)
+    
+    radius_center = pm.datatypes.Vector(adjacent, radius, 0)
+    
+    # calculate the positions on the circle
+    ang_inc = ang_opp/(num_points-1)
+    
+    pos_on_circ = []
+    for i in range(num_points):
+        
+        a = (1.5 * math.pi) - (ang_inc * i)
+        x = math.cos(a) * radius + adjacent
+        y = math.sin(a) * radius + radius
+        pos_on_circ.append(pm.datatypes.Vector(x, y, 0))
+        
+    
+    # reflect the pos on circle across the mid vec  
+    pos_on_circ_reflected = [ 2 * pos_on_circ[i].dot(mid_vec) / mid_vec.dot(mid_vec) * mid_vec - pos_on_circ[i] for i in range(num_points-1) ]
+    
+    # combine the pos and the reflected pos
+    pos_on_circ_reflected.reverse()
+    radius_pos_list =  pos_on_circ + pos_on_circ_reflected
+    
+    
+    # create transformation matrices per point on circle
+    t_matrix_list = []
+    for p in radius_pos_list:
+        aim = (radius_center - p).normal()
+        up = pm.datatypes.Vector(0,0,1)
+        cross = aim.cross(up)
+        
+        tm = pm.datatypes.TransformationMatrix( [ [aim.x, aim.y, aim.z, 0],
+                                                  [cross.x, cross.y, cross.z, 0],
+                                                  [up.x, up.y, up.z, 0],
+                                                  [p.x, p.y, p.z, 1] ])
+        t_matrix_list.append(tm)
+    
+    
+    '''
+    # visualize
+    for i, p in enumerate(radius_pos_list):
+        pos(p)
+        #sp = pm.polySphere(r=.2)[0]
+        #sp.setMatrix(t_matrix_list[i])
+        #pm.toggle(sp, localAxis=True)
+    
+    
+    crv_theta = pm.curve(d=1, p=[(0,0,0),(8,0,0)])
+    crv_ang_diff = pm.curve(d=1, p=[(0,0,0),(8,0,0)])
+       
+    crv_theta.rotate.set((0, 0, pm.util.degrees(theta)))
+    #crv_ang_diff.rotate.set((0, 0, pm.util.degrees(ang_opp)))
+    
+    circ = pm.circle(radius=radius)[0]
+    circ.translate.set(radius_center)
+    
+    pos((adjacent, radius, 0), size=4)
+    '''
+    
+    return t_matrix_list
 
 
-# visualize
-for i, p in enumerate(radius_pos_list):
-    #pos(p)
-    sp = pm.polySphere(r=.2)[0]
-    sp.setMatrix(t_matrix_list[i])
-    pm.toggle(sp, localAxis=True)
+radius_t_matrix_list = build_radius_t_matrix(radius=3, theta=math.pi/6, num_points=4)
 
- 
-crv_theta = pm.curve(d=1, p=[(0,0,0),(8,0,0)])
-crv_ang_diff = pm.curve(d=1, p=[(0,0,0),(8,0,0)])
-   
-crv_theta.rotate.set((0, 0, pm.util.degrees(theta)))
-#crv_ang_diff.rotate.set((0, 0, pm.util.degrees(ang_opp)))
-
-circ = pm.circle(radius=radius)[0]
-circ.translate.set(radius_center)
-
-pos((adjacent, radius, 0), size=4)
-
-
-
+for i, p in enumerate(radius_t_matrix_list):
+        sp = pm.polySphere(r=.2)[0]
+        sp.setMatrix(radius_t_matrix_list[i])
+        pm.toggle(sp, localAxis=True)
