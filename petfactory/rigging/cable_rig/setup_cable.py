206a1025fa81c80f27a401662cd2ec6003f30b2b
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-18 17:43:55 +0100

    add dynamics

--- petfactory/rigging/cable_rig/setup_cable.py
+++ petfactory/rigging/cable_rig/setup_cable.py
@@ -1,8 +1,9 @@
 import pymel.core as pm
 import maya.cmds as cmds
 import pprint
+import petfactory.rigging.nhair.nhair_dynamics as nhair_dynamics
 
-def add_curve_joints(path, num_joints=10, front_axis=0, up_axis=1):
+def add_curve_joints(crv, num_joints=10, name='name', front_axis=0, up_axis=1):
     
     ''' creates joints that are animated along the specified curve'''
 
@@ -26,7 +27,13 @@ def add_curve_joints(path, num_joints=10, front_axis=0, up_axis=1):
     ret_dict['joint_list'] = joint_list
     ret_dict['motionpath_list'] = motionpath_list
 
-    min_u, max_u = path.getShape().getKnotDomain()
+    crv_shape = crv.getShape()
+    result_crv = crv.duplicate(name='result_crv')[0]
+    result_crv_shape = result_crv.getShape()
+    crv_shape.worldSpace[0] >> result_crv_shape.create
+    
+    
+    #min_u, max_u = crv.getShape().getKnotDomain()
     #u_inc = max_u/(num_joints-1)
     u_inc = 1.0/(num_joints-1)
 
@@ -37,7 +44,7 @@ def add_curve_joints(path, num_joints=10, front_axis=0, up_axis=1):
         joint_list.append(jnt)
         
         # create the motion path node, if fractionMode is True it will use 0-1 u range
-        motionpath = pm.pathAnimation(jnt, follow=True, bank=True, fractionMode=True, followAxis=u_front_axis, upAxis=u_up_axis, c=path)
+        motionpath = pm.pathAnimation(jnt, follow=True, bank=True, fractionMode=True, followAxis=u_front_axis, upAxis=u_up_axis, c=crv)
         motionpath_list.append(motionpath)
         
         # break the uvalue anim connection
@@ -48,12 +55,60 @@ def add_curve_joints(path, num_joints=10, front_axis=0, up_axis=1):
         pm.setAttr('{0}.uValue'.format(motionpath), u_inc*index)
   
     
+    
+    num_cvs = crv_shape.numCVs()
+
+    for i in range(num_cvs-2):
+        
+        if i is 0:
+            cv = '0:1'
+            
+        elif i is num_cvs-3:
+            cv = '{0}:{1}'.format(num_cvs-2, num_cvs-1)
+            
+        else:
+            cv = i+1
+        
+        #print(cv) 
+        clust, clust_handle = pm.cluster('{0}.cv[{1}]'.format(crv.longName(), cv), relative=False, name='{0}_{1}_cluster_'.format(name, i))
+        cluster_grp = pm.group(em=True, name='{0}_{1}_cluster_grp'.format(name, i))
+        pm.parent(clust_handle, cluster_grp)
+
+    
+    start_ctrl = pm.circle(name='start_ctrl', normal=(1,0,0))[0]
+    pm.addAttr(start_ctrl, longName='dynamic_blendshape', minValue=0.0, maxValue=1.0, defaultValue=1.0, keyable=True)
+    
+    end_ctrl = pm.circle(name='end_ctrl', normal=(1,0,0))[0]
+    
+    start_ctrl.setMatrix(joint_list[0].getMatrix())
+    end_ctrl.setMatrix(joint_list[-1].getMatrix())
+    
     jnt_grp = pm.group(em=True, name='jnt_grp') 
     pm.parent(joint_list, jnt_grp)
+      
+      
+      
         
+    # make the curves dynamic    
+    nhair_dict_list = nhair_dynamics.make_curve_dynamic(crv)
+    
+    output_curve = nhair_dict_list.get('output_curve')
+    follicle = nhair_dict_list.get('follicle')
+    nucleus = nhair_dict_list.get('nucleus')
+    hairsystem = nhair_dict_list.get('hairsystem')
+    
+    hairsystem.startCurveAttract.set(0.005)
+    nucleus.spaceScale.set(.1)
+    
+    blendshape = pm.blendShape(output_curve, result_crv, origin='world')[0]
+    
+    start_ctrl.dynamic_blendshape >> blendshape.weight[0] 
+
+
+    
     return ret_dict
         
 
 crv = pm.PyNode('curve1')
 
-add_curve_joints(path=crv)
+add_curve_joints(crv=crv)
