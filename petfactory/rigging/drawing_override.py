d4171f9fa31d3ff2c5334bff2371461a44067dc2
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-18 11:33:03 +0100

    enable override

--- petfactory/rigging/drawing_override.py
+++ petfactory/rigging/drawing_override.py
@@ -8,17 +8,17 @@ def maya_main_window():
     main_window_ptr = omui.MQtUtil.mainWindow()
     return wrapInstance(long(main_window_ptr), QtGui.QWidget)
     
-    
+
 # the main ui class  
-class Curve_spreadsheet(QtGui.QWidget):
+class Pet_drawing_override(QtGui.QWidget):
  
     def __init__(self, parent=None):
  
-        super(Curve_spreadsheet, self).__init__(parent)
+        super(Pet_drawing_override, self).__init__(parent)
         self.setWindowFlags(QtCore.Qt.Tool)
         
         self.resize(150, 150)
-        self.setWindowTitle("Pipe Tool")
+        self.setWindowTitle("Set Drawing Overide")
         
         grid = QtGui.QGridLayout()
         grid.setSpacing(1)
@@ -27,23 +27,29 @@ class Curve_spreadsheet(QtGui.QWidget):
         num_buttons = 32
         btn_per_row = 8
 
-        for n in range(num_buttons):
+        for n in range(0, num_buttons):
             
-            color = QtGui.QColor(125, 0, 0)
+            if n is 0:
+                color_float = (.471, .471, .471)
+                
+            else:
+                color_float = pm.colorIndex(n, q=True)
+
+            color = QtGui.QColor()
+
+            color.setRgbF(color_float[0], color_float[1], color_float[2])
             pixmap = QtGui.QPixmap(50, 50)
             pixmap.fill(QtGui.QColor(color))
             icon = QtGui.QIcon(pixmap)
             
-            
             button = QtGui.QPushButton(icon, "")
-            #button.setAutoFillBackground(True)
             
             button.setMinimumSize(50,50)
             button.setMaximumSize(50,50)
             button.setIconSize(QtCore.QSize(50,50))
             button.color_index = n
             button.clicked.connect(self.set_override)
-            grid.addWidget(button, n/btn_per_row, n%btn_per_row)
+            grid.addWidget(button, (n)/btn_per_row, (n)%btn_per_row)
             
 
 
@@ -52,6 +58,7 @@ class Curve_spreadsheet(QtGui.QWidget):
         
     def set_override(self):
         
+        
         sel_list = pm.ls(sl=True)
         
         if not sel_list:
@@ -59,15 +66,17 @@ class Curve_spreadsheet(QtGui.QWidget):
             return
         
         for sel in sel_list:
+
             shape = sel.getShape()
+            if not shape.overrideEnabled.get():
+                shape.overrideEnabled.set(True)
+                
             index = self.sender().color_index
             shape.overrideColor.set(index)
             print('color index set to: \n{0}'.format(index))
 
 
 def show():      
-    win = Curve_spreadsheet(parent=maya_main_window())
+    win = Pet_drawing_override(parent=maya_main_window())
     win.show()
     
-    
-show()
