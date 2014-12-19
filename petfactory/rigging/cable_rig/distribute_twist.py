8d297ea3b610a761416e58eb881aafb5433ce575
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-19 13:38:03 +0100

    add blend twist

--- petfactory/rigging/cable_rig/distribute_twist.py
+++ petfactory/rigging/cable_rig/distribute_twist.py
@@ -2,7 +2,7 @@ import pymel.core as pm
 
 pm.system.openFile('/Users/johan/Documents/Projects/python_dev/scenes/empty_scene.mb', f=True)
 
-num_joints = 10
+num_joints = 5
 
 start_ctrl = pm.circle(name='start', normal=(0,1,0))[0]
 start_ctrl.translate.set(0,num_joints-1,0)
@@ -17,21 +17,41 @@ start_vec_prod.normalizeOutput.set(True)
 start_ctrl.worldMatrix[0] >> start_vec_prod.matrix
 
 
-end_vec_prod = pm.createNode('vectorProduct', name='start_vector_prod')
+end_vec_prod = pm.createNode('vectorProduct', name='end_vector_prod')
 end_vec_prod.operation.set(3)
 end_vec_prod.input1.set(0,0,1)
 end_vec_prod.normalizeOutput.set(True)
 end_ctrl.worldMatrix[0] >> end_vec_prod.matrix
 
 
-sp_start = pm.polySphere(r=.1)[0]
-start_vec_prod.output >> sp_start.translate
+#sp_start = pm.polySphere(r=.1)[0]
+#start_vec_prod.output >> sp_start.translate
 
-sp_end = pm.polySphere(r=.1)[0]
-end_vec_prod.output >> sp_end.translate
+#sp_end = pm.polySphere(r=.1)[0]
+#end_vec_prod.output >> sp_end.translate
 
-for index in range(num_joints):
+# create joints
+joint_list = [ pm.createNode('joint', ss=True) for n in range(num_joints)]
+
+blend_inc = 1.0/(num_joints-1)
+for index, jnt in enumerate(joint_list):
     
-    cube = pm.polyCube(h=.5, d=.5, w=.5)[0]
-    cube.translate.set(0, index, 0)
-    pm.toggle(cube, localAxis=True)
+    print()
+    
+    jnt.translate.set(0, index, 0)
+    pm.toggle(jnt, localAxis=True)
+    
+    blend_colors = pm.createNode('blendColors', name='test')
+    blend_colors.blender.set(blend_inc*index)
+    start_vec_prod.output  >> blend_colors.color1
+    end_vec_prod.output  >> blend_colors.color2
+    
+    if index < num_joints-1:
+        aim_const = pm.aimConstraint(joint_list[index+1], joint_list[index], aimVector=(1,0,0), upVector=(0,0,1), worldUpType='vector', worldUpVector=(0,0,1))
+        
+    else:
+        aim_const = pm.aimConstraint(joint_list[index-1], joint_list[index], aimVector=(-1,0,0), upVector=(0,0,1), worldUpType='vector', worldUpVector=(0,0,1))
+        
+        
+    blend_colors.output >> aim_const.worldUpVector
+        
