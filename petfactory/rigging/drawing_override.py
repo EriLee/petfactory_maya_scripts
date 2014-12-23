32bd765a3b796868745a07f3fad3e81dda1aee7a
Johan Borgström <johan@Johans-Mac-Pro.local>
2014-12-23 12:05:22 +0100

    add option to set to shape

--- petfactory/rigging/drawing_override.py
+++ petfactory/rigging/drawing_override.py
@@ -20,9 +20,19 @@ class Pet_drawing_override(QtGui.QWidget):
         self.resize(150, 150)
         self.setWindowTitle("Set Drawing Overide")
         
+        # layout
+        vertical_layout = QtGui.QVBoxLayout()
+        self.setLayout(vertical_layout)
+        vertical_layout.setContentsMargins(2,2,2,2)
+        
+        # use shape
+        grid_horiz_layout = QtGui.QHBoxLayout()
+        vertical_layout.addLayout(grid_horiz_layout)
+        
         grid = QtGui.QGridLayout()
         grid.setSpacing(1)
-        
+        grid.setContentsMargins(0,0,0,0)
+
 
         num_buttons = 32
         btn_per_row = 8
@@ -53,8 +63,26 @@ class Pet_drawing_override(QtGui.QWidget):
             
 
 
-        self.setLayout(grid)
-        grid.setContentsMargins(2,2,2,2)
+
+        grid_horiz_layout.addLayout(grid)
+        grid_horiz_layout.addStretch()
+        
+        
+        # use shape
+        use_shape_checkbox_horiz_layout = QtGui.QHBoxLayout()
+        vertical_layout.addLayout(use_shape_checkbox_horiz_layout)
+        
+        self.use_shape_checkbox = QtGui.QCheckBox()
+        use_shape_checkbox_horiz_layout.addWidget(self.use_shape_checkbox)
+        
+        shape_checkbox_label = QtGui.QLabel('Set overide on the shape node')
+        use_shape_checkbox_horiz_layout.addWidget(shape_checkbox_label)
+        
+        use_shape_checkbox_horiz_layout.addStretch()
+        
+        vertical_layout.addStretch()
+        
+
         
     def set_override(self):
         
@@ -65,18 +93,41 @@ class Pet_drawing_override(QtGui.QWidget):
             pm.warning('no selection')
             return
         
+        index = self.sender().color_index
+        use_shape = self.use_shape_checkbox.isChecked()
+        
         for sel in sel_list:
-
-            shape = sel.getShape()
-            if not shape.overrideEnabled.get():
-                shape.overrideEnabled.set(True)
+            
+            # if the shape check box is selected add the overide to the shape
+            if use_shape:
+                node = sel.getShape()
                 
-            index = self.sender().color_index
-            shape.overrideColor.set(index)
-            print('color index set to: \n{0}'.format(index))
+            # else use the selected transform
+            else:
+                node = sel
+            
+            
+            # if the indx is 0 remove the override
+            if index is 0:
+                node.overrideColor.set(index)
+                node.overrideEnabled.set(False)
+                print('remove override')
+     
+            # index is not one, set override
+            else:
+                
+                if not node.overrideEnabled.get():
+                    node.overrideEnabled.set(True)
+                
+                
+                node.overrideColor.set(index)
+                
+                print('color index set to: \n{0}'.format(index))
+                    
 
 
 def show():      
     win = Pet_drawing_override(parent=maya_main_window())
     win.show()
     
+show()
