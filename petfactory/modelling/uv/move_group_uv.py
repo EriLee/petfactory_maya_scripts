f7b38b82d984d74a179c4da20b473fd4a32d61dc
Johan Borgström <johan@petfactory.se>
2014-12-29 22:27:37 +0100

    add select child group uvs

--- petfactory/modelling/uv/move_group_uv.py
+++ petfactory/modelling/uv/move_group_uv.py
@@ -11,7 +11,7 @@ def tile_group_uv(grp_list, items_per_row, start_u=0, start_v=0):
     uv_scale = 1.0/items_per_row
     
     for index, grp in enumerate(grp_list):
-        
+                
         # when we have filled one tile create a new list and append to the tile_list
         if index % (items_per_row*items_per_row) is 0:
             temp_list = []
@@ -24,7 +24,7 @@ def tile_group_uv(grp_list, items_per_row, start_u=0, start_v=0):
         v_pos = (((index / items_per_row) % items_per_row) * uv_scale ) + start_v
             
         grp_uvs = pm.polyListComponentConversion(grp, tuv=True)
-        temp_list.append(grp_uvs)
+        temp_list.append({grp.shortName():grp_uvs})
         
         # get the uv min ,max
         (u_min, u_max), (v_min, v_max) = pm.polyEvaluate(grp_uvs, boundingBox2d=True)
@@ -116,44 +116,81 @@ class TileGroupUV(QtGui.QWidget):
         u_start = self.u_start_spinbox.value()
         v_start = self.v_start_spinbox.value()
         
-        tile_list = tile_group_uv(grp_list=grp_list, items_per_row=items_per_row, start_u=u_start, start_v=v_start)
+        main_tile_list = tile_group_uv(grp_list=grp_list, items_per_row=items_per_row, start_u=u_start, start_v=v_start)
         
-        for index, tile in enumerate(tile_list):
+
+        for index, tile_list in enumerate(main_tile_list):
               
-            item = QtGui.QStandardItem('tile {0}'.format(index))
-            
-            custom_data = MyCustomType(tile_list[index])
-            item.setData(custom_data, QtCore.Qt.UserRole + 1)
-                
+            item = QtGui.QStandardItem('Patch {0}'.format(index))
+            main_uv_list = []
             
             # set flags
             item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
             
-            #item.setCheckable(True)
-            #item.setCheckState(QtCore.Qt.Unchecked)
+
+            for row_index, tile_dict in enumerate(tile_list):
+                
+                
+                grp_name = tile_dict.keys()[0]
+                uv_map = tile_dict.get(grp_name)
+                main_uv_list.extend(uv_map)
+                
+                
+                child_item = QtGui.QStandardItem(grp_name)
+                
+                
+                custom_data = MyCustomType(uv_map)
+                
+                child_item.setData(custom_data, QtCore.Qt.UserRole + 1)
+
+                item.setChild(row_index, 0, child_item)
+
+
+        
+            custom_data = MyCustomType(main_uv_list)
+            item.setData(custom_data, QtCore.Qt.UserRole + 1)
+            
             self.model.appendRow(item)
+
             
     
     def select_tile_button_clicked(self):
         
+        root = self.model.invisibleRootItem()
+        num_rows = self.model.rowCount()
         selection_model = self.tree_view.selectionModel()
-        
-        # returns QModelIndex
-        selected_rows = selection_model.selectedRows()
-        
+        selected_indexes = selection_model.selectedIndexes()
+                
+        sel_uv_list = []
+        for row_index in range(num_rows):
+            
+            item = root.child(row_index)
+            q_index = item.index()
+            num_children = item.rowCount()
+            
+            # the parent item is selected
+            if q_index in selected_indexes:
+                #print('add the parent, containing all sub items')
+                
+                data = item.data(QtCore.Qt.UserRole + 1)
+                sel_uv_list.append(data.data)
+                #sel_uv_list.append('PARENT')
+                continue
+                
+            # the parent item is not selected, check if the
+            # child item is selected and if so add to sel_list
+            else:
+                for n in range(num_children):
+                    child = item.child(n)
+                    if child.index() in selected_indexes:
+                        data = child.data(QtCore.Qt.UserRole + 1)
+                        sel_uv_list.append(data.data)
+                        #sel_uv_list.append(child.text())
+                
+        pprint.pprint(sel_uv_list)
         pm.select(deselect=True)
-        sel_tile_list = []
-        for row_index in selected_rows:
-            
-            item = self.model.itemFromIndex(row_index)
-            data = item.data(QtCore.Qt.UserRole + 1)
-            #print(item.text(), data.data)
-            sel_tile_list.extend(data.data)
+        pm.select(sel_uv_list, add=True)
         
-        pprint.pprint(sel_tile_list)   
-        pm.select(sel_tile_list, add=True)
-            
-
            
     @staticmethod
     def add_spinbox(label, parent_layout, min=None, max=None, default=None, double_spinbox=False):
@@ -202,10 +239,11 @@ win.move(150,250)
 
         
 pm.openFile('/Users/johan/Documents/Projects/python_dev/scenes/uv_cube.mb', f=True)
-'''
 
-#grp_list = [pm.PyNode('group{0}'.format(n)) for n in range(15)]
+grp_list = [pm.PyNode('|group{0}'.format(n)) for n in range(15)]
 #pm.select(grp_list)
 #tile_list = tile_group_uv(grp_list=grp_list, items_per_row=2, start_u=0, start_v=0)
 
-#pm.select(tile_list[0])
+pm.select(grp_list)
+win.tile_uvs_button_clicked()
+'''
