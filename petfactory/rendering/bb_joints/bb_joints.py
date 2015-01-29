7704d9cf37466c549ba396b4277b66452bb8d721
Johan Borgström <johan@Johans-Mac-Pro.local>
2015-01-29 10:31:45 +0100

    init bb joints

---
+++ petfactory/rendering/bb_joints/bb_joints.py
@@ -0,0 +1,40 @@
+import pymel.core as pm
+
+
+def create_joints_from_bb(obj, jnt_axis=0, jnt_num=10):
+
+    #xmin ymin zmin xmax ymax zmax.
+    bb = pm.xform(sel_list[0], q=True, bb=True, ws=True)
+    
+    bb_min, bb_max = bb[jnt_axis], bb[jnt_axis+3]
+    x_mid = (bb[3] - bb[0])*.5 + bb[0]
+    y_mid = (bb[4] - bb[1])*.5 + bb[1]
+    z_mid = (bb[5] - bb[2])*.5 + bb[2]
+    
+    dist = bb_max - bb_min
+    jnt_spacing = dist / (jnt_num-1)
+    
+    
+    for x in xrange(jnt_num):
+        
+        if jnt_axis is 0:
+            trans = [(bb_min+jnt_spacing*x), y_mid, z_mid]
+            
+        elif jnt_axis is 1:
+            trans = [x_mid, (bb_min+jnt_spacing*x), z_mid]
+            
+        else:
+            trans = [x_mid, y_mid, (bb_min+jnt_spacing*x)]
+      
+        jnt = pm.createNode('joint', ss=True)
+        jnt.translate.set(trans) 
+
+
+
+
+sel_list = pm.ls(sl=True)
+jnt_axis = 0
+jnt_num = 11
+
+create_joints_from_bb(sel_list[0], jnt_axis, jnt_num)
+
