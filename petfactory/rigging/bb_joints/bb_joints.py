3a7831142cc466ebc86d216f04421af478487f5e
Johan Borgström <johan@Johans-Mac-Pro.local>
2015-01-29 20:16:42 +0100

    add bb_joints init

--- petfactory/rigging/bb_joints/bb_joints.py
+++ petfactory/rigging/bb_joints/bb_joints.py
@@ -1,7 +1,13 @@
 import pymel.core as pm
 
 
-def create_joints_from_bb(obj, jnt_axis=0, jnt_num=10):
+def create_joints_from_bb(jnt_axis=0, jnt_num=10):
+
+    sel_list = pm.ls(sl=True)
+
+    if not sel_list:
+        pm.warning('Nothing is selected')
+        return
 
     #xmin ymin zmin xmax ymax zmax.
     bb = pm.xform(sel_list[0], q=True, bb=True, ws=True)
@@ -27,14 +33,5 @@ def create_joints_from_bb(obj, jnt_axis=0, jnt_num=10):
             trans = [x_mid, y_mid, (bb_min+jnt_spacing*x)]
       
         jnt = pm.createNode('joint', ss=True)
-        jnt.translate.set(trans) 
-
-
-
-
-sel_list = pm.ls(sl=True)
-jnt_axis = 0
-jnt_num = 11
-
-create_joints_from_bb(sel_list[0], jnt_axis, jnt_num)
+        jnt.translate.set(trans)
 
