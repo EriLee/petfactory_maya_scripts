60d852570d48b6973eebfcf18ab98d9e58e1b510
Johan Borgström <johan@Johans-Mac-Pro.local>
2015-01-30 13:19:15 +0100

    add follicle joints

--- petfactory/rigging/ribbon/ribbon.py
+++ petfactory/rigging/ribbon/ribbon.py
@@ -1,10 +1,12 @@
-def build_ribbon(num_joints=10):
+def build_ribbon(width, depth, num_joints=10):
     
     plane_u_patches = num_joints - 1
-    plane_length_ratio = 1.0 / plane_u_patches
-    plane_pivot_pos = plane_u_patches*.5,0,0
+    #plane_length_ratio = 1.0 / plane_u_patches
+    plane_length_ratio = float(depth) / width
+    #plane_pivot_pos = plane_u_patches*.5,0,0
+    plane_pivot_pos = width*.5, 0, 0
     
-    ribbon_plane = pm.nurbsPlane(ax=(0,1,0), n='ribbon_geo', ch=False, patchesU=plane_u_patches, width=plane_u_patches, lengthRatio=plane_length_ratio, pivot=plane_pivot_pos)
+    ribbon_plane = pm.nurbsPlane(ax=(0,1,0), n='ribbon_geo', ch=False, patchesU=plane_u_patches, width=width, lengthRatio=plane_length_ratio, pivot=plane_pivot_pos)
 
     return ribbon_plane[0]
     
@@ -27,16 +29,21 @@ def add_follicles(nurbs_surface, num_follicles=10, direction='u', name='ribbon')
     follicle_grp = pm.group(em=True, n='{0}_follicle_grp'.format(name))
     
     ret_dict = {}
-    follicle_list = []
+    follicle_shape_list = []
+    follicle_transform_list = []
+    
     ret_dict['follicle_grp'] = follicle_grp
-    ret_dict['follicle_list'] = follicle_list
+    ret_dict['follicle_shape_list'] = follicle_shape_list
+    ret_dict['follicle_transform_list'] = follicle_transform_list
         
     
     for n in range(num_follicles):
     
         follicle_shape = pm.createNode('follicle', ss=1, n='{0}_FollicleShape_{1}'.format(name, n))
         follicle_transform = follicle_shape.getParent()
-        follicle_list.append(follicle_shape)
+        
+        follicle_shape_list.append(follicle_shape)
+        follicle_transform_list.append(follicle_transform)
         
         follicle_shape.outRotate >> follicle_transform.rotate
         follicle_shape.outTranslate >> follicle_transform.translate
@@ -57,9 +64,21 @@ def add_follicles(nurbs_surface, num_follicles=10, direction='u', name='ribbon')
     return ret_dict
             
 
-
+def add_follicle_joints(follicle_transform_list):
+    
+    follicle_jnt_grp = pm.group(em=True, n='{0}follicle_jnt_grp')
+    
+    for index, follicle_transform in enumerate(follicle_transform_list):
+        jnt = pm.createNode('joint', ss=True, n='follicle_jnt_{0}'.format(index))
+        pm.parent(jnt, follicle_transform)
+        jnt.translate.set(0,0,0)
+    
+    
 # build the ribbon surface
-ribbon = build_ribbon(num_joints=10)
+ribbon = build_ribbon(width=35, depth=1, num_joints=10)
 
+# add follicles
 follicle_dict = add_follicles(nurbs_surface=ribbon, num_follicles=50)
-#print(follicle_dict.get('follicle_list'))
+
+# add joint to the follicles
+add_follicle_joints(follicle_dict.get('follicle_transform_list'))
