c554cbfa393ffc5ca186168426b25fe830a3bc2f
Johan Borgström <johan@petfactory.se>
2015-02-08 22:16:17 +0100

    separate the creation of udim av coords an the placemet of the uv of the specified nodes

--- petfactory/modelling/uv/uvs_from_index.py
+++ petfactory/modelling/uv/uvs_from_index.py
@@ -15,12 +15,12 @@ and margin estimate.
 # delete transforms
 dev.del_transform()
 
-def uv_from_index_list(num_items_u, num_items_v, num_items, start_index=0, num_random=None):
+def udim_from_index(num_items_u, num_items_v, num_items, start_index=0, num_random=None):
     
     num_items_per_patch = num_items_u * num_items_v
     max_patches_u = 10
 
-    uv_dict = {}
+    udim_dict = {}
     for index in range(start_index, (num_items + start_index)):
         
         # if num_random is specified (int) set the index to be a random
@@ -42,14 +42,54 @@ def uv_from_index_list(num_items_u, num_items_v, num_items, start_index=0, num_r
         udim = 1000 + (patch_index_u + 1) + (max_patches_u * patch_index_v)
 
         # create a key in the dict using the udim
-        if udim not in uv_dict:
-            uv_dict[udim] = []
+        if udim not in udim_dict:
+            udim_dict[udim] = []
 
-        uv_dict[udim].append((u,v))
+        udim_dict[udim].append((u,v))
                 
-    return uv_dict
+    return udim_dict
 
 
+def set_uvs(node_list):
+    
+    
+    
+    #(u_min, u_max), (v_min, v_max) = pm.polyEvaluate(pm.polyListComponentConversion(node_list[0], tuv=True), boundingBox2d=True)
+    #uv_width = u_max - u_min
+    #uv_height = v_max - v_min
+    #padding = 0
+    
+    num_items = len(node_list)
+    
+    # hardcoded values, change later
+    num_items_u = 4
+    num_items_v = 4
+    num_items_per_patch = num_items_u * num_items_v
+    start_index = 0
+    num_random = 16
+    
+    
+    udim_dict = udim_from_index(num_items_u=num_items_u, num_items_v=num_items_v, num_items=num_items, start_index=start_index, num_random=num_random)
+    #pprint.pprint(uv_dict)
+    
+    # step through the udim dict. The dict has the udim as keys with a list of the uv coords as value
+    index = 0
+    for udim in sorted(udim_dict):
+                
+        for uv in udim_dict[udim]:
+            
+            node_uvs = pm.polyListComponentConversion(node_list[index], tuv=True)
+            (u_min, u_max), (v_min, v_max) = pm.polyEvaluate(node_uvs, boundingBox2d=True)
+            
+            u = -u_min + uv[0]
+            v = -v_min + uv[1]
+            
+            pm.polyEditUV(node_uvs, u=u, v=v)
+                                   
+            index += 1
+        
+    
+    
 '''
 def uv_from_index(n):
     
@@ -66,24 +106,24 @@ def uv_from_index(n):
     plane.translate.set(u, v, 0)
 '''
     
+    
+'''
 num_items_u = 2
 num_items_v = 2
 num_items = 40
-start_index = 8
+start_index = 0
 num_random = None
 
-uv_dict = uv_from_index_list(num_items_u=num_items_u, num_items_v=num_items_v, num_items=num_items, start_index=start_index, num_random=num_random)
-#pprint.pprint(uv_dict)
+uv_dict = udim_from_index(num_items_u=num_items_u, num_items_v=num_items_v, num_items=num_items, start_index=start_index, num_random=num_random)
+pprint.pprint(uv_dict)
+'''
 
+pm.openFile("/Users/johan/Documents/Projects/python_dev/scenes/plane_grid.mb", f=True)
+node_list = [pm.PyNode('pPlane{0}'.format(n+1)) for n in range(128)]
+pm.select(node_list)
 
-for udim, uv_list in uv_dict.iteritems():
-    
-    for uv in uv_list:
-        plane = pm.polyPlane(n='UDIM_{0}_{1}{2}'.format(udim, uv[0], uv[1]), axis=(0,0,1), h=1.0/num_items_v, w=1.0/num_items_u, sw=1, sh=1)[0]
-        plane.translate.set(uv[0], uv[1], 0)
-    
+set_uvs(node_list)
 
-#uv_from_index(191)
     
     
     
