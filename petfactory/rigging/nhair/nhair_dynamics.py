777d329b5cb30f5dd8944c2f817e6febb1560d88
Johan Borgström <johan@johans-mac-pro.lan>
2015-02-20 20:46:04 +0100

    update nhair_dynamics

--- petfactory/rigging/nhair/nhair_dynamics.py
+++ petfactory/rigging/nhair/nhair_dynamics.py
@@ -1,48 +1,25 @@
 import pymel.core as pm
+import maya.mel as mel
+#import pprint
+
 
 def make_curve_dynamic(crv):
-    
+
     pm.select(crv)
-    
-    scene_set_before = set(pm.ls())
-    pm.mel.eval('MakeCurvesDynamic')
-    scene_set_after = set(pm.ls())
-    
-    diff_set = scene_set_after.difference(scene_set_before)
+    mel.eval('makeCurvesDynamicHairs 1 0 1;')
     
     ret_dict = {}
-    
-    #pprint.pprint(diff_set)
-    
-    misc_list = [] 
-    ret_dict['misc'] = misc_list
-    
-    for n in diff_set:
-        
-        if isinstance(n, pm.nodetypes.Transform):
-            
-            if isinstance(n.getShape(), pm.nodetypes.Follicle):
-                ret_dict['follicle'] = n
-                #print(n)
-                
-            elif isinstance(n.getShape(), pm.nodetypes.HairSystem):
-                ret_dict['hairsystem'] = n
-                #print(n)
-                
-            elif isinstance(n.getShape(), pm.nodetypes.NurbsCurve):
-                #print(n)
 
-                c = n.getParent()
-                
-                if c != crv:
-                    ret_dict['output_curve'] = n
-                              
-            elif isinstance(n, pm.nodetypes.Nucleus):
-                ret_dict['nucleus'] = n
-                #print(n)
-                
-            else:
-                #print(n)
-                misc_list.append(n)
+    follicle = pm.listRelatives(crv, p=True, pa=True)[0]
+    hairsystem = pm.listConnections( '%s.outHair' % follicle)[0]
+    ret_dict['output_curve'] = pm.listConnections( '%s.outCurve' % follicle)[0]
+    ret_dict['nucleus'] = pm.listConnections( '%s.currentState' % hairsystem)[0]
 
+    ret_dict['follicle'] = follicle
+    ret_dict['hairsystem'] = hairsystem
+    
     return ret_dict
+
+
+#a = make_curve_dynamic('curve1')
+#pprint.pprint(a)
