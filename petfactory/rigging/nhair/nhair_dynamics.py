6c27d832ebe1e5ad44d5d974e99751cb509cb81a
Johan Borgström <johan@petfactory.se>
2015-03-12 17:37:51 +0100

    add param to delete outcurv parent

--- petfactory/rigging/nhair/nhair_dynamics.py
+++ petfactory/rigging/nhair/nhair_dynamics.py
@@ -3,7 +3,7 @@ import maya.mel as mel
 #import pprint
 
 
-def make_curve_dynamic(crv):
+def make_curve_dynamic(crv, delete_out_curve_parent=False):
 
     #pm.select(deselect=True)
     #print(crv)
@@ -13,13 +13,19 @@ def make_curve_dynamic(crv):
     ret_dict = {}
 
     follicle = pm.listRelatives(crv, p=True, pa=True)[0]
+    out_curve = pm.listConnections( '%s.outCurve' % follicle)[0]
     hairsystem = pm.listConnections( '%s.outHair' % follicle)[0]
-    ret_dict['output_curve'] = pm.listConnections( '%s.outCurve' % follicle)[0]
+    ret_dict['output_curve'] = out_curve
     ret_dict['nucleus'] = pm.listConnections( '%s.currentState' % hairsystem)[0]
 
     ret_dict['follicle'] = follicle
     ret_dict['hairsystem'] = hairsystem
-    
+
+    if delete_out_curve_parent:
+        crv_parent = out_curve.getParent()
+        pm.parent(out_curve, world=True)
+        pm.delete(crv_parent)
+
     return ret_dict
 
 
