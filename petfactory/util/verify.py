4baeac6831ed1ffdacb81e2cbc9a8f87a6336f20
Johan Borgström <johan@petfactory.se>
2015-03-19 18:07:23 +0100

    init verift.py

---
+++ petfactory/util/verify.py
@@ -0,0 +1,61 @@
+
+def verify_selection(node_type, use_shape, from_string=False):
+    
+    '''Check if the first object in the selection list is an instance or subclass of the node_type argument.
+    isinstance returns true if the object argument is an instance of the classinfo argument, or of a
+    (direct, indirect or virtual) subclass thereof.
+    
+    usage:
+        print(verify_selection(pm.nodetypes.NurbsCurve, True))
+    '''
+    
+    sel = pm.ls(sl=True)
+        
+    if not sel:
+        pm.warning('Nothing is selected!')
+        
+    if use_shape:
+        
+        try:
+            shape = sel[0].getShape()
+                
+        except AttributeError as e:
+            pm.warning('Select a transform to use the getShape method. {0}'.format(e))
+            return False
+            
+        return isinstance(shape, node_type)
+            
+    else:                 
+        return isinstance(sel[0], node_type)
+
+
+def verify_string(node_name, node_type, use_shape):
+    
+    '''Convert the string to a PyNode and check if it is an instance or subclass of the node_type argument.
+    isinstance returns true if the object argument is an instance of the classinfo argument, or of a
+    (direct, indirect or virtual) subclass thereof.
+    
+    usage:
+        print(verify_string('curve1', pm.nodetypes.Transform, False))
+    '''
+    
+    try:
+        node = pm.PyNode(node_name)
+        
+    except pm.MayaNodeError:
+        pm.warning('Could not create PyNode, {0}'.format(e))
+        return False
+              
+    if use_shape:
+        
+        try:
+            shape = node.getShape()
+                
+        except AttributeError as e:
+            pm.warning('Select a transform to use the getShape method. {0}'.format(e))
+            return False
+            
+        return isinstance(shape, node_type)
+            
+    else:                 
+        return isinstance(node, node_type)
