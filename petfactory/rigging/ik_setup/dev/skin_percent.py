805287ef75a4ea3f3ac8cdeb355d0f644a11b966
Johan Borgström <johan@petfactory.se>
2015-03-27 10:47:49 +0100

    update dev skin percemt

--- petfactory/rigging/ik_setup/dev/skin_percent.py
+++ petfactory/rigging/ik_setup/dev/skin_percent.py
@@ -1,79 +1,79 @@
-def interpolate_positions(pos_list, num_divisions=1):
-    
-    u_inc = 1.0 / (num_divisions+1)
-    last_index = len(pos_list)-1
-    
-    ret_pos_list = []
-    for index, pos in enumerate(pos_list):
+    def interpolate_positions(pos_list, num_divisions=1):
         
-        if index < last_index:
+        u_inc = 1.0 / (num_divisions+1)
+        last_index = len(pos_list)-1
+        
+        ret_pos_list = []
+        for index, pos in enumerate(pos_list):
             
-            dx = pos_list[index+1][0] - pos_list[index][0]
-            dy = pos_list[index+1][1] - pos_list[index][1]
-            dz = pos_list[index+1][2] - pos_list[index][2]
+            if index < last_index:
+                
+                dx = pos_list[index+1][0] - pos_list[index][0]
+                dy = pos_list[index+1][1] - pos_list[index][1]
+                dz = pos_list[index+1][2] - pos_list[index][2]
+                
+                for u in range(num_divisions+1):
+                    ret_pos_list.append((   pos_list[index][0] + dx*u*u_inc,
+                                            pos_list[index][1] + dy*u*u_inc,
+                                            pos_list[index][2] + dz*u*u_inc))
+        
+            else:
+                ret_pos_list.append(pos_list[-1])
+        
+        return ret_pos_list
+        
+        
+    def set_curve_skin_percent(crv, jnt_list, skin_cluster, num_div):
+        
+        crv_shape = crv.getShape()
+        num_jnt = len(jnt_list)
+        
+        if crv_shape.numCVs() is not (num_jnt + (num_jnt-1)*num_div):
+            pm.warning('The number of CVs is not valid')
+            return None
             
-            for u in range(num_divisions+1):
-                ret_pos_list.append((   pos_list[index][0] + dx*u*u_inc,
-                                        pos_list[index][1] + dy*u*u_inc,
-                                        pos_list[index][2] + dz*u*u_inc))
+        # Turn off skinweight normalization and reset skinweights
+        skin_cluster.setNormalizeWeights(0)
+        pm.skinPercent(skin_cluster, crv_shape, nrm=False, prw=100)
+         
+        cv_index = 0
+        u_inc = 1.0 / (num_div + 1)
+        
+        pm.skinPercent(skin_cluster, crv_shape, nrm=False, prw=100)
+        
+        for index in range(num_jnt-1):
+            
+            start = jnt_list[index]
+            end = jnt_list[index+1]
     
-        else:
-            ret_pos_list.append(pos_list[-1])
+            for n in range(num_div+1):
+                u = u_inc * n
+                pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(start), 1.0-u))
+                pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(end), u))
+                cv_index +=1
+                
+        pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(jnt_list[-1]), 1.0))
+        
     
-    return ret_pos_list
     
+    pm.system.openFile('/Users/johan/Documents/Projects/python_dev/scenes/spring_jnts.mb', f=True)
     
-def set_curve_skin_percent(crv, jnt_list, skin_cluster, num_div):
+    ik_jnt_list = [pm.PyNode('joint{0}'.format(j+1)) for j in range(5)]
     
+    jnt_pos_list  = [j.getTranslation(ws=True) for j in ik_jnt_list]
+    
+    num_div = 3
+    
+    pos_list = interpolate_positions(pos_list=jnt_pos_list, num_divisions=num_div)
+    
+    crv = pm.curve(d=1, p=pos_list)
     crv_shape = crv.getShape()
-    num_jnt = len(jnt_list)
     
-    if crv_shape.numCVs() is not (num_jnt + (num_jnt-1)*num_div):
-        pm.warning('The number of CVs is not valid')
-        return None
-        
-    # Turn off skinweight normalization and reset skinweights
-    skin_cluster.setNormalizeWeights(0)
-    pm.skinPercent(skin_cluster, crv_shape, nrm=False, prw=100)
-     
-    cv_index = 0
-    u_inc = 1.0 / (num_div + 1)
+    crv_shape.overrideEnabled.set(1)
+    crv_shape.overrideColor.set(17)
     
-    pm.skinPercent(skin_cluster, crv_shape, nrm=False, prw=100)
+    pm.toggle(crv, controlVertex=True)
     
-    for index in range(num_jnt-1):
-        
-        start = jnt_list[index]
-        end = jnt_list[index+1]
-
-        for n in range(num_div+1):
-            u = u_inc * n
-            pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(start), 1.0-u))
-            pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(end), u))
-            cv_index +=1
-            
-    pm.skinPercent(skin_cluster, '{0}.cv[{1}]'.format(crv, cv_index), transformValue=('{0}'.format(jnt_list[-1]), 1.0))
+    skin_cluster = pm.skinCluster(crv, ik_jnt_list[0])
     
-
-
-pm.system.openFile('/Users/johan/Documents/Projects/python_dev/scenes/spring_jnts.mb', f=True)
-
-ik_jnt_list = [pm.PyNode('joint{0}'.format(j+1)) for j in range(5)]
-
-jnt_pos_list  = [j.getTranslation(ws=True) for j in ik_jnt_list]
-
-num_div = 3
-
-pos_list = interpolate_positions(pos_list=jnt_pos_list, num_divisions=num_div)
-
-crv = pm.curve(d=1, p=pos_list)
-crv_shape = crv.getShape()
-
-crv_shape.overrideEnabled.set(1)
-crv_shape.overrideColor.set(17)
-
-pm.toggle(crv, controlVertex=True)
-
-skin_cluster = pm.skinCluster(crv, ik_jnt_list[0])
-
-set_curve_skin_percent(crv=crv, jnt_list=ik_jnt_list, skin_cluster=skin_cluster, num_div=num_div)
+    set_curve_skin_percent(crv=crv, jnt_list=ik_jnt_list, skin_cluster=skin_cluster, num_div=num_div)
